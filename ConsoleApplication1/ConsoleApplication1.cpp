2bb2f8e7d68ba10ac230c7f4557a717dce1238ee
Zag207 <62791287+Zag207@users.noreply.github.com>
2022-09-17 11:42:52 +0300

    изменил условие на неравенство треугольника

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -21,7 +21,7 @@ int main() {
 	double d2 = sqrt(pow(x1 + x3, 2) + pow(y1 + y3, 2));
 	double d3 = sqrt(pow(x2 + x3, 2) + pow(y2 + y3, 2));
 
-	if (d1 > d2 + d3 && d2 > d1 + d3 && d3 > d1 + d2) {
+	if (d1 >= d2 + d3 && d2 >= d1 + d3 && d3 >= d1 + d2) {
 		std::cout << "Это не треугольник";
 		return 1;
 	}
