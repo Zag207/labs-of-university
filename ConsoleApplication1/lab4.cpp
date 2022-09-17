fcacc1aa7d315de5d6674befa138a67c023ac6d5
Zag207 <62791287+Zag207@users.noreply.github.com>
2022-09-17 11:55:36 +0300

    replace for loop to while loop

--- ConsoleApplication1/lab4.cpp
+++ ConsoleApplication1/lab4.cpp
@@ -4,13 +4,14 @@
 
 int main() {
 	setlocale(LC_ALL, "Rus");
-
+    int i = 11;
     double x, y;
 	std::cout << "Введите x: ";
 	std::cin >> x;
 	
-	for(int i = 11; i > 0; --i){
+	while(i > 0){
 	    y += i*pow(x, (i - 1));
+	    --i;
 	}
 	std::cout <<"Значение выражения: "<<y;
 }
