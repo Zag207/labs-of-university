18d4611eb6a305ff134c65c55fe9cec9b2ed9544
Zag207 <62791287+Zag207@users.noreply.github.com>
2022-09-24 12:21:03 +0300

    Исправил недочет со скобкой
    Добавил обработку случая наличия закрывающей скобки без входной

--- lab9.cpp
+++ lab9.cpp
@@ -9,6 +9,7 @@ int main()
     setlocale(LC_ALL, "Rus");
 
     int count1 = 0, count2 = 0;
+    bool rightOrder = 1;
     std::string str;
     cout << "Введите строку без пробела"<<"\n";
     cin >> str;
@@ -17,9 +18,15 @@ int main()
     {
         count1 += str[i] == '(' ? 1 : 0;
         count2 += str[i] == ')' ? 1 : 0;
+        
+        if (count1 == 0 && str[i] == ')')
+        {
+            rightOrder = 0;
+            break;
+        }
     }
 
-    if (count1 == count2)
+    if (count1 == count2 && rightOrder)
     {
         cout << "Всё хорошо";
     }
